868fe7e6e7a1a8fce2d47bbce2c4ca863e7bcdef
hjacquir <hjacquir@NLYO0323.afssa.fr>
2020-06-18 15:30:35 +0200

    update

--- 2_create_user.bat
+++ 2_create_user.bat
@@ -1,10 +1,14 @@
-                
+                                                                                                                        
 
-Set name="azertuiop"
-Set pass="Bla&12345678"  
+Set filePath=C:\Users\hjacquir\Documents\workspace\ftp-user-creator\users_list.csv
+                                                                                                                                                        
+for /f "usebackq tokens=1-2 delims=;" %%a in (%filePath%) do (
+        echo %%a %%b
+        net user %%a %%b /add /active:yes
+        wmic useraccount WHERE Name='%%a' set PasswordExpires=false
+        wmic useraccount WHERE Name='%%a' set PasswordChangeable=false
+        Echo "utilisateur %%a ajoute avec succes."
+)
 
-net user %name% %pass% /add /active:yes
-wmic useraccount WHERE Name='%name%' set PasswordExpires=false
-wmic useraccount WHERE Name='%name%' set PasswordChangeable=false
+Pause
 
-pause
