9c69fd07cfbe4682e95fc50a6fe0ba08e5c6a337
hjacquir <jacquirhatim@gmail.com>
2020-06-18 12:20:34 +0200

    update

--- 2_create_user.bat
+++ 2_create_user.bat
@@ -1,7 +1,10 @@
-echo off
+                
 
-net user "azertuiop" "Bla&12345678" /add /active:yes
-wmic useraccount WHERE Name='azertuiop' set PasswordExpires=false
-wmic useraccount WHERE Name='azertuiop' set PasswordChangeable=false
+Set name="azertuiop"
+Set pass="Bla&12345678"  
+
+net user %name% %pass% /add /active:yes
+wmic useraccount WHERE Name='%name%' set PasswordExpires=false
+wmic useraccount WHERE Name='%name%' set PasswordChangeable=false
 
 pause
