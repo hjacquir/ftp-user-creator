3c409499b3f043aba4c9beaa3dac79799f12a291
hjacquir <jacquirhatim@gmail.com>
2020-06-19 10:20:22 +0200

    add script batch create ftp user

--- 2_create_user_and_add_to_group.bat
+++ 2_create_user_and_add_to_group.bat
@@ -1,13 +1,20 @@
-                                                                                                                        
+                                                                                 
 
 Set filePath=C:\Users\hjacquir\Documents\workspace\ftp-user-creator\users_list.csv
+Set groupName=hj_ftp_group
                                                                                                                                                         
 for /f "usebackq tokens=1-2 delims=;" %%a in (%filePath%) do (
         echo %%a %%b
+        :: create user
         net user %%a %%b /add /active:yes
+        :: set user password never expire
         wmic useraccount WHERE Name='%%a' set PasswordExpires=false
+        :: set user does not change password
         wmic useraccount WHERE Name='%%a' set PasswordChangeable=false
-        Echo "utilisateur %%a ajoute avec succes."
+        Echo "utilisateur %%a cree avec succes."
+        :: add user to group previously created
+        net localgroup %groupName% %%a /add
+        Echo "utilisateur : %%a ajoute avec succes au groupe : %groupName%."
 )
 
 Pause
